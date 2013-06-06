0b9fb5bcbdc405e0d9284a817a1ade3c68673d5a
jehu <antoine.hars@etu.utc.fr>
2013-06-06 19:37:05 +0200

    td08

---
+++ chatroom/ChatRoom_impl.java
@@ -0,0 +1,26 @@
+// Java ChatRoom_impl.java   
+package chatroom;
+
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class ChatRoom_impl extends ChatRoomPOA {
+
+	public void login_chatroom() {
+
+		System.out.println("login");
+	}
+	
+	public void receive() {
+
+		System.out.println("receive");
+	}
+	
+	public void send() {
+
+		System.out.println("send");
+	}
+}
+
